3d2c4ceaef571a46e61a553f80ab67ca52d81d03
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2024-03-17 12:16:34 -0300

    Metodo Includes

---
+++ MetodoArrays.js
@@ -0,0 +1,16 @@
+// 1º método includes
+// O método includes() determina se um array contém um determinado elemento,
+// Retornando true ou false apropriadamente
+
+// Exemplo 1
+let cor = ['Roxo', 'Rosa', 'amarelo', 'azul', 'verde', 'cinza'];
+
+console.log(cor.includes('azul'));
+// true
+
+
+// Exemplo 2
+let país = ["Rússia", "Espanha", "Brasil", "França"];
+
+console.log(país.includes("Itália"));
+//false
